3f5cbadf9cd873a4a0df73b9813a94b5f8038b32
Erjok2020 <e.mach@alustudent.com>
2023-04-05 10:07:15 +0000

    new

---
+++ api/v1/app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" A flask app to server the API
+"""
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+import os
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": " 0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes the storage on teardown"""
+    storage.close()
+
+
+# 404 error handler
+@app.errorhandler(404)
+def not_found(error):
+    """Returns a JSON-formatted 404 status code response"""
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True, debug=True)
+Footer
